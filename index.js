cc245f799c9bbd861fd50b79801ce862947db486
unknown <payalmehta1606@gmail.com>
2024-03-20 19:18:15 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let themeButton = document.getElementById("theme-button");
+const toggleDarkMode = () => {
+
+  document.body.classList.toggle("dark-mode");
+}
+themeButton.addEventListener("click", toggleDarkMode);
